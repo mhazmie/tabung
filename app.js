3dfefcedd6b1336317e98f16c6b8b1861aa71121
MH <mhazmie96@gmail.com>
2025-06-19 14:30:36 +0800

    v3.0

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
 const connection = require('./db');
 const port = 5001;
 const domain = 'http://localhost:';
