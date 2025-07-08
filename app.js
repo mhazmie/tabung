67d3d2274c8fb8096373b8b8eb5389b3377e45ef
MH <mhazmie96@gmail.com>
2025-07-08 12:30:52 +0800

    v4.6

--- app.js
+++ app.js
@@ -86,8 +86,8 @@ app.get('/spending', isAuthenticated, isAdmin, function (req, res) {
   });
 })
 
-// app.get('/user', isAuthenticated, isAdmin, (req, res) => {
-app.get('/user', (req, res) => {
+app.get('/user', isAuthenticated, isAdmin, (req, res) => {
+  // app.get('/user', (req, res) => {
   var perror = req.query.error;
   var userquery = 'SELECT * FROM users';
   var rolesquery = 'SELECT * FROM roles';
