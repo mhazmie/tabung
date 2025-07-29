98838f5570bf56a5d94596577f3cf739fdb43ea5
mhx <mhazmie96@gmail.com>
2025-07-29 14:09:45 +0800

    v6.5.2

--- middleware/upload.js
+++ middleware/upload.js
@@ -23,7 +23,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
     storage,
     fileFilter,
-    limits: { fileSize: 2 * 1024 * 1024 } // 2MB
+    limits: { fileSize: 5 * 1024 * 1024 } // 5MB
 });
 
 module.exports = upload;
